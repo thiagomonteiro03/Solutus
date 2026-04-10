aa70cebe4d836e0970ebebcfc35c592812b7c90b
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2026-04-10 20:39:21 -0300

    Initial Commit

---
+++ Solutus/SolutusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolutusApp.swift
+//  Solutus
+//
+//  Created by Thiago Monteiro on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SolutusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
